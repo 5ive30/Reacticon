46986f4097d3fdbbc1037e4ff005f01e03fa65e4
5ive30 <5ive30@users.noreply.github.com>
2025-05-30 16:19:57 +0900

    feat(icon): Add icongnbchartpie

---
+++ icons/icongnbchartpie.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+export const icongnbchartpie = () => (
+<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M10.8 9.19997H16.6992C16.6464 5.96564 14.0343 3.35362 10.8 3.30078V9.19997ZM16 10.4999C16 14.0898 13.0899 16.9999 9.5 16.9999C5.91015 16.9999 3 14.0898 3 10.4999C3 6.91009 5.91014 3.99994 9.49999 3.99994V10.4999L16 10.4999Z" fill="#222222"/>
+</svg>
+
+);
